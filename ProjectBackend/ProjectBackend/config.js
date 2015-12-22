40178ef4e4fe7c4c4ea4cde37d683d1f39689ee6
Cyberke <jelle1vdb@hotmail.com>
2015-12-22 12:03:09 +0100

    Basic Project Structure
    * Aanmaak project
    * Config file
    * bin/www aanpassing

---
+++ ProjectBackend/ProjectBackend/config.js
@@ -0,0 +1,22 @@
+﻿/*
+ *  config.js
+ * 
+ */
+
+"use strict";
+
+var config = {
+    HOST: 'http://localhost',
+    PORT: getEnv('PORT') || 3000,
+    
+};
+
+function getEnv(variable){
+    if (process.env[variable] === undefined) {
+        if (variable == 'PORT') { return 1337 };
+        console.log('You must create an environment variable for ' + variable);
+    }
+    return process.env[variable];
+}
+
+module.exports = config;
