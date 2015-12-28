f2cf93006ad714c482fceb5e7ff1dba95732fd3f
Cyberke <jelle1vdb@hotmail.com>
2015-12-28 17:22:31 +0100

    Db config + config + www settings

---
+++ Geofeelingsapp/config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by jelle on 28/12/2015.
+ */
+
+"use strict";
+
+var config = {
+    HOST: 'http://localhost',
+    PORT: getEnv('PORT') || 3000,
+    MONGODBURL: 'mongodb://JelleVDB:abc123@ds049624.mongolab.com:49624/geofeelings'
+};
+
+function getEnv(variable) {
+    if (process.env[variable] === undefined) {
+        if (variable == 'PORT') { return 1337 };
+        console.log('You must create an environment variable for ' + variable);
+    }
+    return process.env[variable]; //of bvb. process.env.WEB_PORT
+};
+
+module.exports = config;
