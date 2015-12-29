c4056bc2291d2de6583127be272cd6930ea4698d
Cyberke <jelle1vdb@hotmail.com>
2015-12-29 15:17:17 +0100

    Validation Error handling op create user

--- Geofeelingsapp/routes/authentication.js
+++ Geofeelingsapp/routes/authentication.js
@@ -14,25 +14,21 @@ router.get('/register', function(req, res){
     res.sendFile(path.join(__dirname, '../public/account/register.html'));
 });
 
-router.post('/register', function(req, res){
+router.post('/register', function(req, res, next){
     //TODO error check
     UsersRepo.createUser(req.body, function(next){
-        //TODO redirect naar login (of autologin) naar homepage
-        res.redirect('/');
+        if(next.errors && next.name === 'ValidationError'){
+            //TODO show validation error
+            res.sendFile(path.join(__dirname, '../public/account/register.html'));
+        }else if(next.errors){
+            next(new Error(next.message));
+        }else{
+            //TODO redirect naar login (of autologin) naar homepage
+            res.redirect('/');
+        }
+
+
     })
-});
-
-router.get('/accounts', function(req, res){
-
-    // Uses Mongoose schema to run the search (empty conditions)
-    var query = User.find({});
-    query.exec(function(err, users){
-        if(err)
-            res.send(err);
-
-        // If no errors are found, it responds with a JSON of all users
-        res.json(users);
-    });
 });
 
 module.exports = router;
